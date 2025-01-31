6a5853e87557b29efb1980d5bc66d683ed455793
AntonV <kave-12@yandex.ru>
2025-01-31 21:28:24 +0400

    v1

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "./config/dotenv.js";
 import authRoutes from "./routes/auth.js";
 import productRoutes from "./routes/products.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -12,17 +13,18 @@ const app = express();
 const port = 3008;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, "client", "dist")));
+//app.use(express.static(path.join(__dirname, "client", "dist")));
 app.use(express.json());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(cookieParser(process.env.ACCESS_TOKEN_SECRET));
 app.use(cookieParser(process.env.REFRESH_TOKEN_SECRET));
 
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 
-app.get("/", function (req, res) {
+/* app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+}); */
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
