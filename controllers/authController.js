82e9239b84cef9f5b5cf28b1cf03c2a2d3fe9317
AntonV <kave-12@yandex.ru>
2025-03-05 14:49:03 +0400

    final

--- controllers/authController.js
+++ controllers/authController.js
@@ -130,7 +130,7 @@ export const updateUser = async (req, res) => {
     const { login, password } = existingUser[0][0];
     const sql = "UPDATE users SET login = ?, password = ?, categories = ? WHERE id = ?";
     const [results] = await pool.execute(sql, [login, password, JSON.stringify(categories), id]);
-    res.send({ id, login, /* password, */ categories });
+    res.send({ id, login, categories });
   } catch (err) {
     console.error("Ошибка обновления пользователя:", err);
     res.status(500).json({ error: "Внутренняя ошибка сервера" });
